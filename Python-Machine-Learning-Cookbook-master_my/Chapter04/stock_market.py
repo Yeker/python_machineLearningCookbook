8ff8247a4a873fbb41d8dee868df512296c22e75
Yeker <ye_zhan_bo@163.com>
2019-02-24 21:48:00 +0800

    update

--- Python-Machine-Learning-Cookbook-master_my/Chapter04/stock_market.py
+++ Python-Machine-Learning-Cookbook-master_my/Chapter04/stock_market.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 from sklearn import covariance, cluster
 # from matplotlib.finance import quotes_historical_yahoo_ochl as quotes_yahoo
 from pandas_datareader import data,wb
-
+import pickle
+import pandas as pd
 
 # Input symbol file
 symbol_file = 'symbol_map.json'
@@ -41,11 +42,25 @@ for symbol in symbols:
 opening_quotes = np.array([quote['Open'] for quote in quotes]).astype(np.float)
 closing_quotes = np.array([quote['Close'] for quote in quotes]).astype(np.float)
 
-# The daily fluctuations of the quotes 
-delta_quotes = closing_quotes - opening_quotes
-
+# The daily fluctuations of the quotes
+# delta_quotes = closing_quotes - opening_quotes
+delta_quotes = opening_quotes
+#使用dump()将数据序列化到文件中
+fw = open('dataFile_opening.txt','wb')
+# Pickle the list using the highest protocol available.
+pickle.dump(delta_quotes, fw, -1)
+# Pickle dictionary using protocol 0.
+pickle.dump(names2, fw)
+fw.close()
+# 使用load()将数据从文件中序列化读出
+fr = open('dataFile_opening.txt','rb')
+delta_quotes = pickle.load(fr)
+names2 = pickle.load(fr)
+fr.close()
 # Build a graph model from the correlations
 edge_model = covariance.GraphLassoCV()
+df = pd.DataFrame (delta_quotes)
+df.to_csv ("stock_data_opening.csv",encoding = "utf-8")
 
 # Standardize the data 
 X = delta_quotes.copy().T
@@ -60,8 +75,12 @@ _, labels = cluster.affinity_propagation(edge_model.covariance_)
 num_labels = labels.max()
 names3_1 = np.array(names2)
 names3_2 = np.array([name[0] for name in names2])
-
+print(labels)
+labels_df = pd.DataFrame (labels)
+labels_df.to_csv("stock_data_opening_labels.csv",encoding = "utf-8")
 # # Print the results of clustering
 for i in range(num_labels + 1):
     print("Cluster", i+1, "-->", ', '.join(names3_2[labels == i]))
+
+
 
